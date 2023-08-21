b989bed83de46b5c7a7c112baa2f8d38f78ee5a3
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-08-21 09:19:36 +0300

    adding sample data

---
+++ data/sample.ts
@@ -0,0 +1,49 @@
+import { NewsProps } from "../types";
+
+export const DUMMY_NEWS: NewsProps[] = [
+  {
+    id: "n1",
+    slug: "will-ai-replace-humans",
+    title: "Will AI Replace Humans?",
+    image: "car.jpg",
+    date: "2022-08-19",
+    content:
+      "Since late 2022 AI is on the rise and therefore many people are losing there job.",
+  },
+  {
+    id: "n2",
+    slug: "movie-review",
+    title: "Movie Review",
+    image: "Inception.jpg",
+    date: "2022-08-19",
+    content:
+      "Since late 2021 AI is on the rise and therefore many people are losing there job.",
+  },
+  {
+    id: "n3",
+    slug: "is-space-travel-real",
+    title: "Is Space Travel Real?",
+    image: "space.jpg",
+    date: "2024-08-19",
+    content:
+      "Since late 2024 AI is on the rise and therefore many people are losing there job.",
+  },
+  {
+    id: "n4",
+    slug: "inception",
+    title: "Inception",
+    image: "Inception.jpg",
+    date: "2020-08-19",
+    content:
+      "Since late 2022 AI is on the rise and therefore many people are losing there job.",
+  },
+  {
+    id: "n5",
+    slug: "are-the-new-cars-better",
+    title: "Are the New Cars Better?",
+    image: "images.jpg",
+    date: "2023-08-19",
+    content:
+      "Since late 2023 AI is on the rise and therefore many people are losing there job.",
+  },
+];
