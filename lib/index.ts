715d766f52b5f050a5acd9758a4f068837cfbad2
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-08-23 10:17:11 +0300

    adding getLatestNews

--- lib/index.ts
+++ lib/index.ts
@@ -2,15 +2,28 @@ import { DUMMY_NEWS } from "../data/sample";
 export const getFullYear = () => {
   const newsArr = DUMMY_NEWS;
   const years: number[] = [];
-  newsArr.map((news) => {
+
+  newsArr.forEach((news) => {
     const year = new Date(news.date).getFullYear();
     years.push(year);
   });
-  return years.sort((a, b) => b - a);
+
+  const uniqueYears = Array.from(new Set(years));
+  return uniqueYears.sort((a, b) => b - a);
 };
+
 export const getYearFilteredNews = (year: string) => {
   const filteredNews = DUMMY_NEWS.filter((news) => {
     return new Date(news.date).getFullYear() === Number(year);
   });
   return filteredNews;
+};
+
+export const getLatestNews = () => {
+  const newsArr = DUMMY_NEWS.slice();
+  newsArr.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+  const latestNews = newsArr.slice(0, 3);
+  return latestNews;
 };
