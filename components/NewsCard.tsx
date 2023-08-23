05dd17b46db235a830a8134546180a3d514cd785
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-08-23 10:20:45 +0300

    modfying the newscard ui

--- components/NewsCard.tsx
+++ components/NewsCard.tsx
@@ -1,25 +1,36 @@
 import Image from "next/image";
 import Link from "next/link";
 import { NewsProps } from "../types";
+
 const NewsCard = ({ news }: { news: NewsProps }) => {
   const { title, image, slug, date, content } = { ...news };
+
   return (
-    <Link href={`news/${slug}`}>
-      <div className='img'>
-        <Image
-          src={`/news/${image}`}
-          width={300}
-          height={300}
-          alt={title}
-          priority
-        />
-      </div>
-      <div className='contents'>
-        <p>{title}</p>
-        <span>{date}</span>
-        <p>{content}</p>
-      </div>
-    </Link>
+    <div className='bg-white rounded-lg shadow-lg overflow-hidden'>
+      <Link
+        href={`news/${slug}`}
+        className='block hover:opacity-75 transition duration-300 ease-in-out'
+      >
+        <div className='relative h-60 overflow-hidden'>
+          <Image
+            src={`/news/${image}`}
+            layout='fill'
+            objectFit='cover'
+            alt={title}
+            priority
+          />
+        </div>
+        <div className='p-4'>
+          <h3 className='text-xl font-semibold text-gray-800 mb-2 truncate'>
+            {title}
+          </h3>
+          <p className='text-gray-600 text-sm mb-2 line-clamp-2'>{content}</p>
+          <time className='text-gray-400 text-sm'>
+            {new Date(date).toLocaleDateString()}
+          </time>
+        </div>
+      </Link>
+    </div>
   );
 };
 
