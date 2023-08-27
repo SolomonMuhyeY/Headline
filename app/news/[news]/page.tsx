ad5747ee04378bf0516ac88c32b3437f0e6204b1
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-08-27 12:22:20 +0300

    adding key and modfying Image

--- app/news/[news]/page.tsx
+++ app/news/[news]/page.tsx
@@ -18,14 +18,13 @@ const NewsDetailPage = ({
   return (
     <div className='max-w-3xl mx-auto px-4 py-8'>
       <h1 className='text-3xl font-bold mb-4'>{newsItem.title}</h1>
-      <div className='relative h-80 mb-4'>
+      <div className='relative h-80 w-3/4 mb-4'>
         <Image
           src={`/news/${newsItem.image}`}
-          layout='fill'
-          objectFit='cover'
           alt={newsItem.title}
-          priority
-          className='rounded-lg'
+          width={500}
+          height={500}
+          className='rounded-lg w-full h-full'
         />
       </div>
       <div className='flex items-center text-gray-500 mb-4'>
