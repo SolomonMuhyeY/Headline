09009e44702d19787c9c81528db27a4280aab415
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-08-30 12:24:48 +0300

    modfying layout

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -3,10 +3,10 @@ import NewsCard from "../../components/NewsCard";
 
 const NewsPage = () => {
   const newsItem = DUMMY_NEWS.map((news) => (
-    <NewsCard key={news.id} news={news} />
+    <NewsCard key={crypto.randomUUID()} news={news} />
   ));
   return (
-    <div className='w-9/10 sm:w-3/4 mx-auto grid sm:grid-cols-2 gap-8'>
+    <div className='w-9/10 sm:w-3/4 mx-auto grid md:grid-cols-2 gap-8'>
       {newsItem}
     </div>
   );
