aa272cdddbc6e1923f40fd60f5c8938c3b618a3c
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-09-01 14:38:11 +0300

    adding getMonthsForYear method

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,5 @@
-import { DUMMY_NEWS } from "../data/sample";
+import { DUMMY_NEWS } from "@/data/sample";
+
 export const getFullYear = () => {
   const newsArr = DUMMY_NEWS;
   const years: number[] = [];
@@ -12,17 +13,53 @@ export const getFullYear = () => {
   return uniqueYears.sort((a, b) => b - a);
 };
 
-export const getYearFilteredNews = (year: string) => {
+export const getNewsForYear = (year: string) => {
   const filteredNews = DUMMY_NEWS.filter((news) => {
     return new Date(news.date).getFullYear() === Number(year);
   });
   return filteredNews;
 };
 
+export const getMonthsForYear = (year: string): string[] => {
+  const allNews = DUMMY_NEWS;
+  const selectedMonths: Set<number> = new Set();
+
+  allNews.forEach((news) => {
+    const newsDate = new Date(news.date);
+    if (newsDate.getFullYear() === Number(year)) {
+      selectedMonths.add(newsDate.getMonth() + 1); // Months are zero-based, so add 1
+    }
+  });
+
+  const sortedMonths: number[] = Array.from(selectedMonths).sort(
+    (a: number, b: number) => a - b
+  );
+
+  const monthNames: string[] = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const monthNamesSorted: string[] = sortedMonths.map(
+    (month) => monthNames[month - 1]
+  ); // Convert month numbers to names
+
+  return monthNamesSorted; // Return array of month names
+};
+
 export const getLatestNews = () => {
   const newsArr = DUMMY_NEWS.slice();
   newsArr.sort(
-    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+    (a: any, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
   );
   const latestNews = newsArr.slice(0, 3);
   return latestNews;
