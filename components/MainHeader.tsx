358d27897f668840fd5b52d26881ea16edc91724
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-09-29 15:16:21 +0300

    modfying the styling

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -1,21 +1,63 @@
+"use client";
 import Link from "next/link";
-import "./mainHeader.css";
-import React from "react";
+import React, { useState } from "react";
 import NavLink from "./NavLink";
+
 const MainHeader = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className='nav'>
-      <ul>
-        <li>
-          <Link href='/'>Home</Link>
-        </li>
-        <li>
+    <nav className='bg-gray-800 p-4 fixed w-full top-0 z-10'>
+      <div className='container mx-auto flex justify-between items-center'>
+        <div className='text-white text-2xl font-bold'>
+          <Link href='/'>HeadLine</Link>
+        </div>
+        <div className='hidden md:flex space-x-4'>
           <NavLink href='/news'>News</NavLink>
-        </li>
-        <li>
           <NavLink href='/archive'>Archive</NavLink>
-        </li>
-      </ul>
+        </div>
+        <div className='md:hidden'>
+          <button onClick={() => setIsOpen(!isOpen)} className='text-white'>
+            {isOpen ? (
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 w-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M6 18L18 6M6 6l12 12'
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 w-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M4 6h16M4 12h16m-7 6h7'
+                />
+              </svg>
+            )}
+          </button>
+        </div>
+      </div>
+      {isOpen && (
+        <div className='md:hidden flex flex-col space-y-4 mt-4'>
+          <NavLink href='/news'>News</NavLink>
+          <NavLink href='/archive'>Archive</NavLink>
+        </div>
+      )}
     </nav>
   );
 };
