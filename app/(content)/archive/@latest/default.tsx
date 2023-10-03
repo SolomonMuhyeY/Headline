44819003effa7507c391cfb7662de09710743a57
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-10-03 16:12:56 +0300

    modfying the styling

--- app/(content)/archive/@latest/default.tsx
+++ app/(content)/archive/@latest/default.tsx
@@ -4,12 +4,17 @@ import NewsCard from "../../../../components/NewsCard";
 
 const LatestNewsPage = () => {
   const latestNews = getLatestNews();
+
   return (
-    <div>
-      <h1>Latest News</h1>
-      {latestNews.map((news) => (
-        <NewsCard key={crypto.randomUUID()} news={news} />
-      ))}
+    <div className='container mx-auto px-4 py-8'>
+      <h1 className='text-3xl font-bold mb-8'>Latest News</h1>
+      <div className='grid grid-cols-1 mx-12 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        {latestNews.map((news) => (
+          <div key={news.id}>
+            <NewsCard news={news} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
