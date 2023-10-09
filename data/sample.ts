88254999fa5274e5a6447495a540570cecd209b9
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-10-09 18:32:29 +0300

    modfying sample data

--- data/sample.ts
+++ data/sample.ts
@@ -3,47 +3,110 @@ import { NewsProps } from "../types";
 export const DUMMY_NEWS: NewsProps[] = [
   {
     id: "n1",
-    slug: "will-ai-replace-humans",
-    title: "Will AI Replace Humans?",
-    image: "car.jpg",
-    date: "2022-08-19",
+    slug: "ai-and-automation",
+    title: "AI and Automation: The Future of Work",
+    image: "ai_automation.jpg",
+    date: "2023-01-15",
     content:
-      "Since late 2022 AI is on the rise and therefore many people are losing there job.",
+      "Artificial Intelligence (AI) and automation are transforming industries, raising questions about the future of work and the role of human workers.",
   },
   {
     id: "n2",
-    slug: "movie-review",
-    title: "Movie Review",
-    image: "Inception.jpg",
-    date: "2022-08-19",
+    slug: "climate-change-update",
+    title: "Climate Change Update: Rising Global Temperatures",
+    image: "climate_change.jpg",
+    date: "2023-04-22",
     content:
-      "Since late 2021 AI is on the rise and therefore many people are losing there job.",
+      "Recent reports show a significant rise in global temperatures, prompting renewed calls for urgent climate action.",
   },
   {
     id: "n3",
-    slug: "is-space-travel-real",
-    title: "Is Space Travel Real?",
-    image: "space.jpg",
-    date: "2024-08-19",
+    slug: "tech-giants-in-congress",
+    title: "Tech Giants Face Congress: Privacy and Data Issues",
+    image: "tech_giants.jpg",
+    date: "2023-06-10",
     content:
-      "Since late 2024 AI is on the rise and therefore many people are losing there job.",
+      "Tech giants testify before Congress on privacy concerns and data handling practices amidst growing public scrutiny.",
   },
   {
     id: "n4",
-    slug: "inception",
-    title: "Inception",
-    image: "Inception.jpg",
-    date: "2020-08-19",
+    slug: "future-of-space-exploration",
+    title: "The Future of Space Exploration: New Frontiers",
+    image: "space_exploration.jpg",
+    date: "2023-08-28",
     content:
-      "Since late 2022 AI is on the rise and therefore many people are losing there job.",
+      "Exploration beyond Earth continues to expand with new missions and advancements in space technology.",
   },
   {
     id: "n5",
-    slug: "are-the-new-cars-better",
-    title: "Are the New Cars Better?",
-    image: "images.jpg",
-    date: "2023-08-19",
+    slug: "cryptocurrency-boom",
+    title: "Cryptocurrency Boom: Digital Assets Reach New Highs",
+    image: "cryptocurrency.jpg",
+    date: "2023-10-05",
     content:
-      "Since late 2023 AI is on the rise and therefore many people are losing there job.",
+      "Cryptocurrencies surge in value as investors flock to digital assets amid economic uncertainties.",
+  },
+  {
+    id: "n6",
+    slug: "rise-of-remote-work",
+    title: "The Rise of Remote Work: Shaping the Future of Employment",
+    image: "remote_work.jpg",
+    date: "2023-11-12",
+    content:
+      "Remote work becomes a permanent fixture in many industries, transforming workplace dynamics globally.",
+  },
+  {
+    id: "n7",
+    slug: "future-of-ai-ethics",
+    title: "The Future of AI Ethics: Balancing Innovation and Responsibility",
+    image: "ai_ethics.jpg",
+    date: "2024-01-20",
+    content:
+      "Debates intensify over the ethical implications of AI technologies as they become more integrated into daily life.",
+  },
+  {
+    id: "n8",
+    slug: "olympics-2024-highlights",
+    title: "Tokyo Olympics 2024: Memorable Moments and Achievements",
+    image: "olympics_2024.jpg",
+    date: "2024-08-17",
+    content:
+      "The Tokyo Olympics 2024 witness remarkable performances and inspiring stories from athletes around the world.",
+  },
+  {
+    id: "n9",
+    slug: "future-of-electric-vehicles",
+    title: "The Future of Electric Vehicles: Sustainable Transportation",
+    image: "electric_vehicles.jpg",
+    date: "2024-10-30",
+    content:
+      "Electric vehicles continue to gain popularity as governments and automakers invest in sustainable transportation solutions.",
+  },
+  {
+    id: "n10",
+    slug: "advancements-in-medical-research",
+    title: "Advancements in Medical Research: Breakthroughs and Innovations",
+    image: "medical_research.jpg",
+    date: "2024-12-08",
+    content:
+      "Medical researchers achieve significant breakthroughs in treatments and therapies, offering new hope for patients worldwide.",
+  },
+  {
+    id: "n11",
+    slug: "future-of-5g-technology",
+    title: "The Future of 5G Technology: Connectivity and Innovation",
+    image: "5g_technology.jpg",
+    date: "2025-02-14",
+    content:
+      "5G technology promises faster speeds and greater connectivity, paving the way for revolutionary applications across industries.",
+  },
+  {
+    id: "n12",
+    slug: "global-economic-recovery",
+    title: "Global Economic Recovery: Trends and Challenges Ahead",
+    image: "global_economy.jpg",
+    date: "2025-04-01",
+    content:
+      "Countries navigate the complexities of economic recovery post-pandemic, facing both opportunities and obstacles.",
   },
 ];
