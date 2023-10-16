e51ea8025dc24d5b8c91e04321f6bcc3a1f194d5
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-10-16 19:41:45 +0300

    refactoring the code

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,4 +1,5 @@
 import NewsCard from "@/components/NewsCard";
+import RelatedNews from "@/components/RelatedNews";
 import { DUMMY_NEWS } from "@/data/sample";
 
 const NewsPage = () => {
@@ -11,33 +12,14 @@ const NewsPage = () => {
   return (
     <div className='container mx-auto px-4 py-8'>
       <h1 className='text-3xl font-bold mb-4'>Latest News</h1>
-      <div className='grid md:grid-cols-12 gap-5'>
+      <div className='grid md:grid-cols-12 gap-5 relative'>
         {/* Column for Related News */}
         <div className='col-span-3 order-2 md:order-none md:sticky md:top-20'>
-          <div className='md:sticky md:top-20'>
-            <h2 className='text-xl text-left font-bold mb-2'>Related News</h2>
-            <ul className='space-y-2'>
-              <li className='flex items-center hover:bg-gray-200 hover:text-gray-800 p-2 rounded-md cursor-pointer'>
-                List Item One
-              </li>
-              <li className='flex items-center hover:bg-gray-200 hover:text-gray-800 p-2 rounded-md cursor-pointer'>
-                List Item Two
-              </li>
-              <li className='flex items-center hover:bg-gray-200 hover:text-gray-800 p-2 rounded-md cursor-pointer'>
-                List Item Three
-              </li>
-              <li className='flex items-center hover:bg-gray-200 hover:text-gray-800 p-2 rounded-md cursor-pointer'>
-                List Item Four
-              </li>
-              <li className='flex items-center hover:bg-gray-200 hover:text-gray-800 p-2 rounded-md cursor-pointer'>
-                List Item Five
-              </li>
-              <li className='flex items-center hover:bg-gray-200 hover:text-gray-800 p-2 rounded-md cursor-pointer'>
-                List Item Six
-              </li>
-            </ul>
-          </div>
+          <RelatedNews />
         </div>
+
+        {/* Vertical Line */}
+        <div className='hidden md:block fixed left-96 top-0 bottom-0 h-full border-l-2 border-gray-700'></div>
 
         {/* Column for News Items */}
         <div className='grid mx-12 md:grid-cols-1 col-span-9 lg:grid-cols-2 gap-8'>
