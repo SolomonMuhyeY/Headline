a63527226f3ca0b50dc11d3f18581eadf6e79c2f
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-10-16 19:39:59 +0300

    modfying the styling

--- app/(content)/archive/@archive/[[...slug]]/error.tsx
+++ app/(content)/archive/@archive/[[...slug]]/error.tsx
@@ -7,7 +7,7 @@ const Error = ({
   };
 }) => {
   return (
-    <div className='flex flex-col items-center justify-center  bg-red-50 text-red-800 p-4'>
+    <div className='flex py-12 flex-col items-center justify-center  bg-gray-900 text-red-800 p-4'>
       <div className='bg-white shadow-lg rounded-lg p-6 max-w-md text-center'>
         <h2 className='text-3xl font-bold mb-4'>Error Occurred!</h2>
         <p className='text-lg'>{error.message}</p>
