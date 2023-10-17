1b1e796e386c995dff4bc91acab35c63a21a417e
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-10-17 23:01:56 +0300

    adding loading skeleton

--- components/SmallNewsCard.tsx
+++ components/SmallNewsCard.tsx
@@ -1,28 +1,45 @@
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { NewsProps } from "../types";
+import SmallNewsCardSkeleton from "./skeleton/SmallNewsCardSkeleton";
+
+import { NewsProps } from "@/types";
 
 const SmallNewsCard = ({ news }: { news: NewsProps }) => {
   const { title, image, slug } = news;
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading effect or fetch data if necessary
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000); // Adjust delay as needed
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div className='bg-white w-5/6 sm:w-full rounded-lg shadow-lg overflow-hidden'>
-      <Link href={`news/${slug}`} className='block'>
-        <div className='relative h-24'>
-          <Image
-            src={`/news/${image}`}
-            alt={title}
-            width={500}
-            height={500}
-            className='object-cover w-full h-full'
-          />
-        </div>
-        <div className='p-4'>
-          <h3 className='text-xs px-2 break-words font-semibold text-gray-800 mb-2'>
-            {title}
-          </h3>
-        </div>
-      </Link>
+      {isLoading ? (
+        <SmallNewsCardSkeleton />
+      ) : (
+        <Link href={`news/${slug}`} passHref>
+          <div className='relative h-28'>
+            <Image
+              src={`/news/${image}`}
+              alt={title}
+              width={500}
+              height={500}
+              className='object-cover w-full h-full'
+            />
+          </div>
+          <div className='p-4'>
+            <h3 className='text-xs px-2 break-words font-semibold text-gray-800 mb-2'>
+              {title}
+            </h3>
+          </div>
+        </Link>
+      )}
     </div>
   );
 };
