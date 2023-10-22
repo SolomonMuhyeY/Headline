2076c1794e8628ba7fd6db779f026d0f06677db3
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-10-22 07:08:21 +0300

    modfying the data fetching

--- components/RelatedNews.tsx
+++ components/RelatedNews.tsx
@@ -1,10 +1,11 @@
-import { RELATED_NEWS } from "@/data/sample";
 import SmallNewsCard from "./SmallNewsCard";
+import { getRelatedNews } from "@/lib";
 
 const RelatedNews = () => {
-  const relatedNews = RELATED_NEWS.map((news) => (
-    <SmallNewsCard key={news.id} news={news} />
-  ));
+  const relatedNewsData = getRelatedNews();
+  const relatedNews = relatedNewsData.map((news) => {
+    return <SmallNewsCard key={news.id} news={news} />;
+  });
   return (
     <div className='-mt-16 col-span-8 md:col-span-3 order-2 px-12 md:px-2 md:order-none md:sticky md:top-20'>
       <h2 className='text-xl mb-8 text-left font-bold '>Related News</h2>
