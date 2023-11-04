b3b3fdd7a47added29ea15318574111dab07206e
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-04 09:37:47 +0300

    modfying the styling

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -33,19 +33,19 @@ const NewsPage = () => {
   };
 
   return (
-    <div className='container mx-auto px-4 py-8'>
+    <div className='container mx-auto px-4 py-8 overflow-x-hidden'>
       <h1 className='text-3xl font-bold mb-4'>Latest News</h1>
-      <div className='grid md:grid-cols-12 gap-5 relative'>
+      <div className='relative grid md:grid-cols-12 gap-5'>
         {/* Column for Related News */}
-        <div className='col-span-3 order-2 md:order-none md:sticky md:top-20'>
+        <div className='col-span-5 md:col-span-3 order-2 md:order-none md:sticky md:top-20'>
           <RelatedNews />
         </div>
 
         {/* Vertical Line */}
-        <div className='hidden md:block fixed left-96 top-0 bottom-0 h-full border-l-2 border-gray-700'></div>
+        <div className='hidden xl:block fixed left-96 top-0 bottom-0 h-full border-l-2 border-gray-700'></div>
 
         {/* Column for News Items */}
-        <div className='grid mx-12 md:grid-cols-1 col-span-9 lg:grid-cols-2 gap-8'>
+        <div className='grid md:mx-12 col-span-9 md:grid-cols-1  lg:grid-cols-2 gap-8'>
           {renderNewsItems()}
         </div>
       </div>
