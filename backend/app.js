9bd2f42b2f543ece06c7354d107316a1917f5cd1
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-07 12:22:14 +0300

    phase 02: adding backend | replacing the dummy data

---
+++ backend/app.js
@@ -0,0 +1,251 @@
+import sqlite from "better-sqlite3";
+import express from "express";
+import cors from "cors";
+
+const DUMMY_NEWS = [
+  {
+    id: "n1",
+    slug: "ai-and-automation",
+    title: "AI and Automation: The Future of Work",
+    image: "ai_automation.jpg",
+    date: "2023-01-15",
+    content:
+      "Artificial Intelligence (AI) and automation are transforming industries, raising questions about the future of work and the role of human workers.",
+  },
+  {
+    id: "n2",
+    slug: "climate-change-update",
+    title: "Climate Change Update: Rising Global Temperatures",
+    image: "climate_change.jpg",
+    date: "2023-04-22",
+    content:
+      "Recent reports show a significant rise in global temperatures, prompting renewed calls for urgent climate action.",
+  },
+  {
+    id: "n3",
+    slug: "tech-giants-in-congress",
+    title: "Tech Giants Face Congress: Privacy and Data Issues",
+    image: "tech_giants.jpg",
+    date: "2023-06-10",
+    content:
+      "Tech giants testify before Congress on privacy concerns and data handling practices amidst growing public scrutiny.",
+  },
+  {
+    id: "n4",
+    slug: "future-of-space-exploration",
+    title: "The Future of Space Exploration: New Frontiers",
+    image: "space_exploration.jpg",
+    date: "2023-08-28",
+    content:
+      "Exploration beyond Earth continues to expand with new missions and advancements in space technology.",
+  },
+  {
+    id: "n5",
+    slug: "cryptocurrency-boom",
+    title: "Cryptocurrency Boom: Digital Assets Reach New Highs",
+    image: "cryptocurrency.jpg",
+    date: "2023-10-05",
+    content:
+      "Cryptocurrencies surge in value as investors flock to digital assets amid economic uncertainties.",
+  },
+  {
+    id: "n6",
+    slug: "rise-of-remote-work",
+    title: "The Rise of Remote Work: Shaping the Future of Employment",
+    image: "remote_work.jpg",
+    date: "2023-11-12",
+    content:
+      "Remote work becomes a permanent fixture in many industries, transforming workplace dynamics globally.",
+  },
+  {
+    id: "n7",
+    slug: "future-of-ai-ethics",
+    title: "The Future of AI Ethics: Balancing Innovation and Responsibility",
+    image: "ai_ethics.jpg",
+    date: "2024-01-20",
+    content:
+      "Debates intensify over the ethical implications of AI technologies as they become more integrated into daily life.",
+  },
+  {
+    id: "n8",
+    slug: "olympics-2024-highlights",
+    title: "Tokyo Olympics 2024: Memorable Moments and Achievements",
+    image: "olympics_2024.jpg",
+    date: "2024-08-17",
+    content:
+      "The Tokyo Olympics 2024 witness remarkable performances and inspiring stories from athletes around the world.",
+  },
+  {
+    id: "n9",
+    slug: "future-of-electric-vehicles",
+    title: "The Future of Electric Vehicles: Sustainable Transportation",
+    image: "electric_vehicles.jpg",
+    date: "2024-10-30",
+    content:
+      "Electric vehicles continue to gain popularity as governments and automakers invest in sustainable transportation solutions.",
+  },
+  {
+    id: "n10",
+    slug: "advancements-in-medical-research",
+    title: "Advancements in Medical Research: Breakthroughs and Innovations",
+    image: "medical_research.jpg",
+    date: "2024-12-08",
+    content:
+      "Medical researchers achieve significant breakthroughs in treatments and therapies, offering new hope for patients worldwide.",
+  },
+  {
+    id: "n11",
+    slug: "future-of-5g-technology",
+    title: "The Future of 5G Technology: Connectivity and Innovation",
+    image: "5g_technology.jpg",
+    date: "2025-02-14",
+    content:
+      "5G technology promises faster speeds and greater connectivity, paving the way for revolutionary applications across industries.",
+  },
+  {
+    id: "n12",
+    slug: "global-economic-recovery",
+    title: "Global Economic Recovery: Trends and Challenges Ahead",
+    image: "global_economy.jpg",
+    date: "2025-04-01",
+    content:
+      "Countries navigate the complexities of economic recovery post-pandemic, facing both opportunities and obstacles.",
+  },
+];
+const RELATED_NEWS = [
+  {
+    id: "rn1",
+    slug: "future-of-robotics",
+    title: "The Future of Robotics: Innovations and Applications",
+    image: "future_robotics.jpg",
+    date: "2025-06-18",
+    content:
+      "Advancements in robotics revolutionize industries with new automated solutions and collaborative robots.",
+  },
+  {
+    id: "rn2",
+    slug: "blockchain-technology",
+    title: "Blockchain Technology: Beyond Cryptocurrencies",
+    image: "blockchain.jpg",
+    date: "2025-08-02",
+    content:
+      "Blockchain evolves beyond cryptocurrencies, offering secure and decentralized solutions across various sectors.",
+  },
+  {
+    id: "rn3",
+    slug: "future-of-artificial-intelligence",
+    title: "The Future of Artificial Intelligence: Trends and Innovations",
+    image: "future_ai.jpg",
+    date: "2025-09-20",
+    content:
+      "AI continues to shape industries with advances in machine learning, natural language processing, and robotics.",
+  },
+  {
+    id: "rn4",
+    slug: "renewable-energy-technologies",
+    title: "Renewable Energy Technologies: Sustainable Solutions",
+    image: "renewable_energy.jpg",
+    date: "2025-11-12",
+    content:
+      "Renewable energy technologies play a crucial role in addressing climate change and promoting sustainable development.",
+  },
+  {
+    id: "rn5",
+    slug: "future-of-space-tourism",
+    title: "The Future of Space Tourism: Commercial Spaceflights",
+    image: "space_tourism.jpg",
+    date: "2026-01-05",
+    content:
+      "Space tourism industry expands with commercial spaceflights offering unique experiences beyond Earth's atmosphere.",
+  },
+];
+const db = sqlite("news_DB.db");
+
+function initDB() {
+  db.prepare(
+    `
+    CREATE TABLE IF NOT EXISTS news(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      slug TEXT NOT NULL,
+      title TEXT NOT NULL,
+      image TEXT NOT NULL,
+      date TEXT NOT NULL,
+      content TEXT NOT NULL
+    )
+  `
+  ).run();
+
+  const stmt = db.prepare(`
+    INSERT INTO news(slug, title, content, date, image) VALUES (
+      @slug,
+      @title,
+      @content,
+      @date,
+      @image
+    )
+  `);
+
+  const insert = db.transaction((newsList) => {
+    for (const news of newsList) {
+      stmt.run(news);
+    }
+  });
+
+  insert(DUMMY_NEWS);
+  db.prepare(
+    `
+    CREATE TABLE IF NOT EXISTS related_news(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      slug TEXT NOT NULL,
+      title TEXT NOT NULL,
+      image TEXT NOT NULL,
+      date TEXT NOT NULL,
+      content TEXT NOT NULL
+    )
+  `
+  ).run();
+
+  const stmt2 = db.prepare(`
+    INSERT INTO related_news(slug, title, content, date, image) VALUES (
+      @slug,
+      @title,
+      @content,
+      @date,
+      @image
+    )
+  `);
+
+  const insert2 = db.transaction((newsList) => {
+    for (const news of newsList) {
+      stmt2.run(news);
+    }
+  });
+
+  insert2(RELATED_NEWS);
+}
+
+// using express
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/news", (req, res) => {
+  try {
+    const news = db.prepare("SELECT * FROM news").all();
+    res.json(news);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+app.get("/related-news", (req, res) => {
+  try {
+    const related_news = db.prepare("SELECT * FROM related_news").all();
+    res.json(related_news);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+initDB();
+app.listen(8080, () => {
+  console.log("Listening on Port 8080...");
+});
