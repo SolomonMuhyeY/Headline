2a6b693166ad01f87cf3da6acd8e1953568d1129
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-11 12:44:07 +0300

    phase 01: modfying the styling

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -17,7 +17,7 @@ const NewsPage = async () => {
   ));
 
   return (
-    <div className='container mx-auto px-4 py-8 overflow-x-hidden'>
+    <div className='container mx-auto px-4 py-8'>
       <h1 className='text-3xl font-bold mb-4'>Latest News</h1>
       <div className='relative grid md:grid-cols-12 gap-5'>
         {/* Column for Related News */}
@@ -29,7 +29,7 @@ const NewsPage = async () => {
         <div className='hidden xl:block fixed left-96 top-0 bottom-0 h-full border-l-2 border-gray-700'></div>
 
         {/* Column for News Items */}
-        <div className='grid md:mx-12 col-span-9 md:grid-cols-1  lg:grid-cols-2 gap-8'>
+        <div className='grid md:mx-12 overflow-x-hidden col-span-9 md:grid-cols-1  lg:grid-cols-2 gap-8'>
           {news}
         </div>
       </div>
